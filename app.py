51e9d9311c1cbd877d4585afcd3c5a59c55d6ae6
Ubuntu <ubuntu@ip-10-0-14-29.eu-west-3.compute.internal>
2023-02-05 18:22:00 +0000

    Initial comment

---
+++ app.py
@@ -0,0 +1,72 @@
+from flask import Flask, jsonify, render_template
+import pymongo
+from pymongo import MongoClient
+import smtplib
+
+# Connect to MongoDB
+client = MongoClient("mongodb://username:password@host:port/")
+db = client.mydatabase
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('./index.html')
+
+def test(): #0
+    pass
+def show_menu(): #1
+    pass
+def show_how(): #2
+    pass
+def battle_short(): #3
+    pass
+def clear_scr(): #4
+    pass
+def submit(): #5
+    pass
+
+functions = {
+    0: test,
+    1: show_menu,
+    2: show_how,
+    3: battle_short,
+    4: clear_scr,
+    5: submit
+}
+
+def main():
+    show_menu()
+    
+    pass
+if __name__ == '__main__':
+    app.run(debug=True)
+
+@app.route("/get_data", methods=["GET"])
+def get_data():
+    # Get data from MongoDB
+    data = db.mycollection.find_one({"tail_number": "ABC123"})
+    return jsonify(data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+db.mycollection.update_one(
+    {"tail_number": "ABC123"},
+    {"$inc": {"amount": -1}}
+)
+db.mycollection.update_one(
+    {"tail_number": "ABC123"},
+    {"$set": {"status": "maintenance"}}
+)
+to = "example@email.com"
+subject = "Plane Maintenance"
+body = "The plane with tail number ABC123 is now in maintenance mode."
+
+email_text = f"Subject: {subject}\n\n{body}"
+
+with smtplib.SMTP("smtp.gmail.com", 587) as server:
+    server.ehlo()
+    server.starttls()
+    server.login("sender@gmail.com", "password")
+    server.sendmail("sender@gmail.com", to, email_text)
